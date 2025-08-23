d869403883b5b7a0c90e3b63b4bab76973f60a8e
Querty <10-Querty@users.noreply.gitlab.valtitude.xyz>
2025-08-23 21:57:22 +0200

    ezkpodfjaezodieza

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         },
       ]
     }
-
+    
     if (isProduction) {
       return [
         {
@@ -49,6 +49,26 @@ const nextConfig = {
         ],
       },
     ]
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://altitrace.reachaltitude.xyz' : '',
+  async redirects() {
+    if (process.env.NODE_ENV === 'production') {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'header',
+              key: 'x-forwarded-proto',
+              value: 'http',
+            },
+          ],
+          destination: 'https://altitrace.reachaltitude.xyz/:path*',
+          permanent: true,
+        },
+      ]
+    }
+    return []
   },
 }
 
